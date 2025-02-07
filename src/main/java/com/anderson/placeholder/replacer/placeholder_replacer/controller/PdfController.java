bbb65dc4ff06268572af350f5e5512ebcb4f7fb3
andylou <anderson1.louis@gmail.com>
2025-02-06 21:58:48 -0500

    - created a test endpoint to ensure that I can connect it through a browser

--- src/main/java/com/anderson/placeholder/replacer/placeholder_replacer/controller/PdfController.java
+++ src/main/java/com/anderson/placeholder/replacer/placeholder_replacer/controller/PdfController.java
@@ -1,9 +1,18 @@
 package com.anderson.placeholder.replacer.placeholder_replacer.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/pdf")
 public class PdfController {
+
+    @GetMapping("/replace")
+    public ResponseEntity<String> replacePlaceholder()
+    {
+        return ResponseEntity.ok("PDF updated successfully");
+    }
+
 }
