b91ce820a955923591d7abb39043c4a0a0f139a4
andylou <anderson1.louis@gmail.com>
2025-02-06 19:31:32 -0500

    Initial commit

---
+++ src/main/java/com/anderson/placeholder/replacer/placeholder_replacer/PlaceholderReplacerApplication.java
@@ -0,0 +1,13 @@
+package com.anderson.placeholder.replacer.placeholder_replacer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlaceholderReplacerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlaceholderReplacerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/anderson/placeholder/replacer/placeholder_replacer/PlaceholderReplacerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anderson.placeholder.replacer.placeholder_replacer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlaceholderReplacerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
