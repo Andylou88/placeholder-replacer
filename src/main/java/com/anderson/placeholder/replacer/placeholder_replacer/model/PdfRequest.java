18f72e1e3bc511cd400faab992dd4003f5bfd4b3
andylou <anderson1.louis@gmail.com>
2025-02-06 23:17:29 -0500

    - removed setters because they were not used

--- src/main/java/com/anderson/placeholder/replacer/placeholder_replacer/model/PdfRequest.java
+++ src/main/java/com/anderson/placeholder/replacer/placeholder_replacer/model/PdfRequest.java
@@ -10,31 +10,15 @@ public class PdfRequest {
         return placeholder;
     }
 
-    public void setPlaceholder(String placeholder) {
-        this.placeholder = placeholder;
-    }
-
     public String getReplacement() {
         return replacement;
-    }
-
-    public void setReplacement(String replacement) {
-        this.replacement = replacement;
     }
 
     public String getInputPdfPath() {
         return inputPdfPath;
     }
 
-    public void setInputPdfPath(String inputPdfPath) {
-        this.inputPdfPath = inputPdfPath;
-    }
-
     public String getOutputPdfPath() {
         return outputPdfPath;
-    }
-
-    public void setOutputPdfPath(String outputPdfPath) {
-        this.outputPdfPath = outputPdfPath;
     }
 }
